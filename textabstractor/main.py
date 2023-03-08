e954e1702d9df788228805cc5aa1d6ef601c9351
schorndorfer <will.k.t@gmail.com>
2023-03-07 22:37:17 -0600

    adding error handling and logging

--- textabstractor/main.py
+++ textabstractor/main.py
@@ -1,8 +1,10 @@
 import requests
+import logging
+import logging.handlers
 import json
 from rich import print
 from importlib_resources import files
-from fastapi import BackgroundTasks, FastAPI
+from fastapi import BackgroundTasks, FastAPI, HTTPException
 from fastapi.encoders import jsonable_encoder
 import textabstractor_testdata.breast as data
 from textabstractor import textabstract
@@ -15,6 +17,18 @@ from textabstractor.dataclasses import (
 
 # --------------------------------------------------------------------------------------------------
 app = FastAPI()
+
+
+# --------------------------------------------------------------------------------------------------
+LOG_FILENAME = 'nlp_service.log'
+logger = logging.getLogger(__name__)
+logging.basicConfig(
+    filename=LOG_FILENAME,
+    format='%(asctime)s : %(levelname)s : %(message)s',
+    level=logging.INFO)
+handler = logging.handlers.RotatingFileHandler(
+    LOG_FILENAME, maxBytes=2_000_0000, backupCount=5)
+logger.addHandler(handler)
 
 
 # --------------------------------------------------------------------------------------------------
@@ -46,16 +60,23 @@ async def greeting():
 def post_suggestions(
     abstractor_suggestions_uri: str, suggestions: SuggestionSet
 ) -> bool:
-    resp = requests.post(abstractor_suggestions_uri, json=jsonable_encoder(suggestions))
-    return resp.ok
+    try:
+        resp = requests.post(abstractor_suggestions_uri, json=jsonable_encoder(suggestions))
+        return resp.ok
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
 
 
 # --------------------------------------------------------------------------------------------------
 @app.post("/multiple_suggest", status_code=202)
 def multiple_suggest(background_tasks: BackgroundTasks, request: SuggestRequest):
-    background_tasks.add_task(handle_request, request=request)
-    return {"status": "accepted", "request.source_id": request.source_id}
-
+    try:
+        logger.info(f"request: {request.source_id}: {request.text[:100]}")
+        background_tasks.add_task(handle_request, request=request)
+        return {"status": "accepted", "request.source_id": request.source_id}
+    except Exception as e:
+        logger.error(f"multiple_suggest: {e}")
+        raise HTTPException(status_code=500, detail=str(e))
 
 # --------------------------------------------------------------------------------------------------
 @app.get(
